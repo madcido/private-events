c5df24bc3f1cdd870f4ddd12a9f6a44300efb2c7
Dan Montiel <danmontielh@gmail.com>
2019-05-17 15:52:19 -0500

    relations implemented and views for users and events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,12 +1,30 @@
 class EventsController < ApplicationController
 
   def create
+    @user = User.find_by(id: session[:id])
+    @event = @user.events.build(events_params)
+    if @event.save
+      flash["success"] = "Event created"
+      redirect_to @event
+    else
+      flash["error"] = "Event not created"
+      render :index
+    end
   end
 
   def show
+    @event = Event.find_by(id: params[:id])
   end
 
   def index
+    @event = Event.new()
+    @events = Event.all
+  end
+
+  private
+
+  def events_params
+    params.require(:event).permit(:name, :description, :date)
   end
   
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_17_181213) do
+ActiveRecord::Schema.define(version: 2019_05_17_192704) do
 
   create_table "events", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 2019_05_17_181213) do
     t.date "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "creator_id"
+    t.index ["creator_id"], name: "index_events_on_creator_id"
   end
 
   create_table "users", force: :cascade do |t|
